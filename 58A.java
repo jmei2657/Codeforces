e60a50afa67e88a5a4eb476da2dd17529801b462
jmei2657 <panda2jzn@gmail.com>
2023-01-31 00:13:04 -0500

    Added 58A problem (1000 problem)

---
+++ 58A.java
@@ -0,0 +1,28 @@
+package Codeforces;
+
+import java.util.Scanner;
+
+/*
+ Start: 11:14 pm
+ End: 11:40 pm
+ */
+public class Main {
+    public static void main(String[] args) {
+        String hello = "hello";
+        int helloIndex = 0;
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        int sIndex = 0;
+        while (sIndex < s.length()) {
+            if (hello.charAt(helloIndex) == s.charAt(sIndex)) {
+                helloIndex++;
+                if (helloIndex == 5) {
+                    System.out.println("YES");
+                    return;
+                }
+            }
+            sIndex++;
+        }
+        System.out.println("NO");
+    }
+}
